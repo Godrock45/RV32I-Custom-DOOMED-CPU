c69edff05c9debc43ee4ffa5d4cf0eacf2b2782b
Godrock45 <akshat2045@gmail.com>
2026-07-23 19:05:41 -0400

    Auto-commit: registers.sv

--- registers.sv
+++ registers.sv
@@ -1,9 +1,12 @@
 module registers(
     input clk,
     input we,
-    input [4:0] rs
+    input [4:0] rs1,
+    input [4:0] rs2,
     input [4:0]rd,
     input [31:0]wd
+    output [4:0] rd1,
+    output [4:0] rd2
 );
 
 
