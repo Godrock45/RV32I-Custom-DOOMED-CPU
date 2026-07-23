bbe7ef7fc81e24e6db60d7c81e1fabab92f332ba
Godrock45 <akshat2045@gmail.com>
2026-07-23 19:53:30 -0400

    Auto-commit: registers.sv

--- registers.sv
+++ registers.sv
@@ -5,10 +5,17 @@ module registers(
     input [4:0] rs2,
     input [4:0]rd,
     input [31:0]wd
-    output [4:0] rd1,
-    output [4:0] rd2
+    output logic [31:0] rd1,
+    output logic [31:0] rd2
 );
-
+logic [31:0] registers[31:0];
+always @ begin
+    if(we)begin
+        registers[rd]=wd;
+    end
+    rd1=registers[rs1];
+    rd2=registers[rs2];
+end
 
 
 endmodule
