aa2b706bdba7935d29c768aebb5453772d649829
Godrock45 <akshat2045@gmail.com>
2026-07-23 18:48:28 -0400

    Auto-commit: ALU.sv

--- ALU.sv
+++ ALU.sv
@@ -7,18 +7,18 @@ module ALU(
 
 always_comb begin
     case(ALUCtrl)
-    1000: Res=OpA+OpB;
-    1001: Res=OpA-OpB;
-    0001: Res=OpA << (OpB[5:0]);
-    0010: Res=$signed(OpA)<$signed(OpB);
-    0011: Res=OpA<OpB;
-    0100: Res= OpA^OpB;
-    0101: Res= OpA >> OpB[5:0];
-    1101: Res= {OpA[31],OpA[30:0]>>OpB[4:0]};
-    0110: Res= OpA|OpB;
-    0111: Res= OpA&OpB;
+    1000: Res=OpA+OpB;                          //ADD
+    1001: Res=OpA-OpB;                          //SUB
+    0001: Res=OpA << (OpB[4:0]);                //SLL
+    0010: Res={31'b0,$signed(OpA)<$signed(OpB)};//SLT
+    0011: Res=OpA<OpB;                          //SLTU
+    0100: Res= OpA^OpB;                         //XOR
+    0101: Res= OpA >> OpB[4:0];                 //SRL
+    1101: Res= {OpA[31],OpA[30:0]>>OpB[4:0]};   //SRA
+    0110: Res= OpA|OpB;                         //OR
+    0111: Res= OpA&OpB;                         //AND
     default:
-    Res=0;
+    Res=32'b0;
     endcase
     
 end
