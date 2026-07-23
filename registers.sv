2526236377b19ce16827e04a4c326a3d2aac3bd2
Godrock45 <akshat2045@gmail.com>
2026-07-23 19:57:16 -0400

    Auto-commit: registers.sv

--- registers.sv
+++ registers.sv
@@ -1,20 +1,31 @@
 module registers(
     input clk,
+    input rst,
     input we,
     input [4:0] rs1,
     input [4:0] rs2,
     input [4:0]rd,
-    input [31:0]wd
+    input [31:0]wd,
     output logic [31:0] rd1,
     output logic [31:0] rd2
 );
 logic [31:0] registers[31:0];
-always @ begin
-    if(we)begin
-        registers[rd]=wd;
-    end
+always_comb begin
     rd1=registers[rs1];
     rd2=registers[rs2];
+end
+assign registers[0]=32'b0;
+
+always_ff @( posedge clk ) begin
+    if(rst)begin
+        [31:0] registers<=32'b0;
+    end
+    else if(we) begin
+        registers[rd]<=wd;
+    end
+
+
+    
 end
 
 
