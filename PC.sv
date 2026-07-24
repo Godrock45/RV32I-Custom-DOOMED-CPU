23a1dca53a6a890fa454d7af62f1303e4d11fa78
Godrock45 <akshat2045@gmail.com>
2026-07-24 09:46:40 -0400

    Auto-commit: PC.sv

---
+++ PC.sv
@@ -0,0 +1,24 @@
+module PrgCo(
+    input clk,
+    input rst,
+    input pc_en;
+    input [31:0]next_pc,
+    output logic [31:0]pc
+);
+always @(posedge clk ) begin
+    if(rst)begin
+        pc<=0;
+    end
+    else if(pc_en) begin
+        pc<=next_pc;
+    end
+    else begin
+        pc<=pc;
+    end
+end
+
+
+
+
+
+endmodule
