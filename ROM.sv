5dcc0603e17b0d68ac91aca5d0baf3ccb9505a7b
Godrock45 <akshat2045@gmail.com>
2026-07-24 10:05:34 -0400

    Auto-commit: ROM.sv

--- ROM.sv
+++ ROM.sv
@@ -2,8 +2,11 @@ module ROME(
     input [31:0]PC,
     output [31:0] IR
 );
- reg [31:0] registan [255:0];
-assign IR=registan[PC[7:0]];
+initial begin
+    $readmemh("program.hex",registan);
+end
+reg [31:0] registan [255:0];
+assign IR=registan[PC[31:2]];
 
 
 endmodule
