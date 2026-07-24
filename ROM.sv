8f2e722444f92513b2e9ce547fe0f4438554002c
Godrock45 <akshat2045@gmail.com>
2026-07-24 10:02:40 -0400

    Auto-commit: ROM.sv

--- ROM.sv
+++ ROM.sv
@@ -1,11 +1,9 @@
 module ROME(
-    input [31:0]PC;
-    output [31:0] IR;
+    input [31:0]PC,
+    output [31:0] IR
 );
- reg [255:0] registan [31:0];
-always_comb begin
-    IR<=registan[PC];
-end
+ reg [31:0] registan [255:0];
+assign IR=registan[PC[7:0]];
 
 
 endmodule
