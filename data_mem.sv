c4baf7d950448dec90babb0c2873d0b5fa37b3e1
Godrock45 <akshat2045@gmail.com>
2026-07-25 15:26:40 -0400

    Auto-commit: data_mem.sv

---
+++ data_mem.sv
@@ -0,0 +1,24 @@
+module memory(
+    input [31:0]addr,
+    input [31:0]dat,
+    input write_ena,
+    output logic [31:0]mem_dat
+);
+reg [31:0] memistan [255:0]
+always_comb begin
+    mem_dat=memistan[addr[9:2]]
+end
+
+always_ff @(posedge clk) begin
+    if(write_ena) begin
+        memistan[addr[9:2]]=dat;
+    end
+end
+
+
+
+
+
+
+
+endmodule
