327278d83efc376329e085c0aff679dc796fd6f2
Godrock45 <akshat2045@gmail.com>
2026-07-27 09:12:13 -0400

    Auto-commit: top.sv

---
+++ top.sv
@@ -0,0 +1,30 @@
+module top_wire(
+    input clk,
+    input rst,
+    //debug assist
+    output logic [31:0] dbg_pc,
+    output logic [31:0] dbg_instr,
+    output logic dbg_reg_we,
+    output logic [4:0] dbg_rd,
+    output logic [31:0] dbg_wb_data,
+    output logic halt
+    );
+
+    reg [31:0] PC_loc;
+
+
+
+
+
+
+
+
+
+
+    PrgCo ad(.)
+
+
+
+
+
+endmodule
