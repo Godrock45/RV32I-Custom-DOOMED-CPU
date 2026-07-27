982385cf9f02a13b66df2cd82e101bcd3b1e8565
Godrock45 <akshat2045@gmail.com>
2026-07-26 23:23:46 -0400

    Auto-commit: data_mem.sv

--- data_mem.sv
+++ data_mem.sv
@@ -2,7 +2,7 @@ module memory(
     input clk,
     input [31:0]addr,
     input [31:0]dat,
-    input logic [1:0]funct3,
+    input logic [2:0]funct3,
     input write_ena,
     output logic [31:0]mem_dat
 );
@@ -15,10 +15,10 @@ always_ff @(posedge clk) begin
     if(write_ena) begin
         case(funct3)
         3'b000:begin
-            memistan[addr[9:2]][7:0]<=dat[7:0];
+            memistan[addr[9:2]][8*addr[1:0]+:8]<=dat[7:0];
         end
         3'b001:begin
-            memistan[addr[9:2]][15:0]<=dat[15:0];
+            memistan[addr[9:2]][16*addr[1]+:16]<=dat[15:0];
         end
         3'b010:begin
             memistan[addr[9:2]]<=dat;
