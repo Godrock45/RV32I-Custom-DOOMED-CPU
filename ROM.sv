8c2c7e37cb8e68541278085cf314bc2a3889c789
Godrock45 <akshat2045@gmail.com>
2026-07-31 11:51:54 -0400

    Auto-commit: TestBench_basic.sv

--- ROM.sv
+++ ROM.sv
@@ -4,7 +4,15 @@ module ROME(
 );
 reg [31:0] registan [255:0];
 initial begin
-    $readmemh("program.hex",registan);
+    registan[0]=32'h00500093;
+    registan[1]=32'h00300113;
+    registan[2]=32'h002081B3;
+    registan[3]=32'h00302023;
+    registan[4]=32'h00002203;
+    registan[5]=32'h00418463;
+    registan[6]=32'h00000013;
+    registan[7]=32'h00000063;
+
 end
 assign IR=registan[PC[9:2]];
 
--- TestBench_basic.sv
+++ TestBench_basic.sv
@@ -6,7 +6,7 @@ logic clk,rst;
 logic [31:0] dbg_pc,dbg_instr,dbg_wb_data;
 logic [4:0] dbg_rd;
 logic dbg_reg_we, halt;
-top_wire dut(.clk(clk),.rst(rst),.dbg_pc(dbg_pc),.dbg_instr(dbg_instr),.dbg_rd(dbg_rd),.dbg_reg_we(dbg_reg_we),.dbg_wb_data(dbg_wb_data),.halt(halt);
+top_wire dut(.clk(clk),.rst(rst),.dbg_pc(dbg_pc),.dbg_instr(dbg_instr),.dbg_rd(dbg_rd),.dbg_reg_we(dbg_reg_we),.dbg_wb_data(dbg_wb_data),.halt(halt));
 
 initial clk=0;
 always #5 clk=~clk;
@@ -18,6 +18,11 @@ initial begin
     rst=0;
   #2000 $finish;
 end
+
+always @(posedge clk) if (!rst && dbg_reg_we && dbg_rd != 0)
+    $display("%0t pc=%08h instr=%08h x%0d <= %08h",
+             $time, dbg_pc, dbg_instr, dbg_rd, dbg_wb_data);
+             
 initial $dumpfile("wave.vcd");
 initial $dumpvars(0,test1);
 
